e51ba26aef7f45fdf96ddf194af9bd116913c485
darylE <derwin12@users.noreply.github.com>
2023-12-10 16:19:49 -0500

    model type with pixel count

--- Source Files/app/main/models.py
+++ Source Files/app/main/models.py
@@ -35,22 +35,31 @@ def find_exact(type, model, fgi, fmi, xLightsImportModelNodes):
     return None
 
 
-def find_similar(type, model, fgi, fmi):
+def find_model_type(type, model, fgi, fmi):
     """Compare model type to find potential match
-        need to do similar groups
+        go thru the possible matches to find the best one
     """
+    candidates = []
     for m in fmi:
         if model['displayas'] == m['displayas']:
-            logging.info(f"FOUND SIMILAR! ({type}) {model['name']} ({model['displayas']}) with {m['name']}")
-            return m
+            logging.debug(f"FOUND MODEL TYPE Candidate! ({type}) {model['name']} #{model['pixelcount']} ({model['displayas']}) with {m['name']} #{m['pixelcount']}")
+            candidates.append(m)
+    if len(candidates) > 0:
+        diff = 9999
+        candidate = None
+        for c in candidates:
+            if abs(c['pixelcount'] - model['pixelcount']) < diff:
+                candidate = c
+                diff = abs(c['pixelcount'] - model['pixelcount'])
+        logging.info(f"FOUND MODEL TYPE! ({type}) {model['name']} ({model['displayas']}) with {candidate['name']}")
+        # Find one with the closest pixel count
+        return candidate
     return None
 
 
 def is_matching(target_str, str_list):
     # Convert the target string to lowercase and remove spaces
     target_str_normalized = target_str.lower().replace(" ", "")
-    if target_str == "Candy Canes-02":
-        logging.info("stop")
     logging.debug(f"is_matching: {target_str}: {target_str_normalized} with {str_list}")
     logging.debug(f'Results {any(target_str_normalized.startswith(s.lower().replace(" ", "")) for s in str_list)}')
 
@@ -58,7 +67,24 @@ def is_matching(target_str, str_list):
     return any(target_str_normalized.startswith(s.lower().replace(" ", "")) for s in str_list)
 
 
-def find_pattern(type, model, fgi, fmi):
+def find_fuzzy(type, model, fgi, fmi):
+    """Compare fuzzy model name to find potential match
+        need to do similar groups
+    """
+    for g in fgi:
+        logging.debug(f"Fuzzy match (group) {g['name']} with {model['name']}")
+        if is_matching(model['name'], [g['name']]):
+            logging.info(f"FOUND FUZZY! ({type}) {model['name']} with {g['name']}")
+            return g
+    return None
+    for m in fmi:
+        logging.debug(f"Fuzzy match {m['name']} with {model['name']}")
+        if is_matching(model['name'], [m['name']]):
+            logging.info(f"FOUND FUZZY! ({type}) {model['name']} with {m['name']}")
+            return m
+    return None
+
+def find_111(type, model, fgi, fmi):
     """Compare model type to find potential match
         need to do similar groups
     """
@@ -74,6 +100,7 @@ def find_pattern(type, model, fgi, fmi):
             logging.info(f"FOUND PATTERN! ({type}) {model['name']} with {m['name']}")
             return m
     return None
+
 
 def find_hint(model, xLightsImportModelNodes):
     """
@@ -92,7 +119,7 @@ def find_match(type, model, fgi, fmi, xLightsImportModelNodes):
         if val == None:
             val = find_exact(type, model, fgi, fmi, xLightsImportModelNodes)
             if val == None:
-                val = find_similar(type, model, fgi, fmi)
+                val = find_model_type(type, model, fgi, fmi)
                 if val == None:
                     val = random.choice(fgi)
     else:
@@ -100,12 +127,14 @@ def find_match(type, model, fgi, fmi, xLightsImportModelNodes):
         if val == None:
             val = find_exact(type, model, fgi, fmi, xLightsImportModelNodes)
             if val == None:
-                val = find_similar(type, model, fgi, fmi)
+                val = find_model_type(type, model, fgi, fmi)
                 if val == None:
-                    val = find_pattern(type, model, fgi, fmi)
+                    val = find_fuzzy(type, model, fgi, fmi)
                     if val == None:
-                        logging.info(f"NOT FOUND! for {model['name']}")
-                        val = random.choice(fgi)
+                        val = find_111(type, model, fgi, fmi)
+                        if val == None:
+                            logging.info(f"***NOT FOUND .. random! for {model['name']}")
+                            val = random.choice(fgi)
     logging.debug(f"Groups {fgi}, Models {fmi}")
     logging.info(f"find_match results: \"{model['name']}\" is mapped to \"{val['name']}\"")
     return val
